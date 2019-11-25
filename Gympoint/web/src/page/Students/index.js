884ec6fc55d56f7eba03126a201ee926885b98db
Henrique <henrique.campaner@live.com>
2019-11-25 22:26:08 +0000

    student list

--- Gympoint/web/src/page/Students/index.js
+++ Gympoint/web/src/page/Students/index.js
@@ -1,16 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdPersonAdd } from 'react-icons/md';
 import api from '~/services/api';
 
-import { Container, ContentHead, StudentsList } from './styles';
+import {
+  Container,
+  ContentHead,
+  StudentsList,
+  BtnEdit,
+  BtnDelete,
+} from './styles';
 
 export default function Students() {
-  useEffect(() => {
-    const response = api.get('students');
+  const [students, setStudents] = useState([]);
 
-    console.tron.log(response.data);
+  useEffect(() => {
+    async function loadStudents() {
+      const { data } = await api.get('students');
+      setStudents(data);
+    }
+
+    loadStudents();
   }, []);
+
+  async function handleDelete(id) {
+    // eslint-disable-next-line no-alert
+    if (window.confirm('Do you want to delete this student?') === true) {
+      await api.delete(`students/${id}`);
+
+      setStudents(students.filter(student => student.id !== id));
+    }
+  }
 
   return (
     <Container>
@@ -25,7 +45,39 @@ export default function Students() {
           <input placeholder="Buscar aluno" />
         </aside>
       </ContentHead>
-      <StudentsList>oi</StudentsList>
+      <StudentsList>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>E-mail</th>
+              <th>Age</th>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {students.map(item => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.age}</td>
+                <td>
+                  <BtnEdit type="BtnEdit">Editar</BtnEdit>
+                  <BtnDelete
+                    type="button"
+                    onClick={() => {
+                      handleDelete(item.id);
+                    }}
+                  >
+                    Apgar
+                  </BtnDelete>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </StudentsList>
     </Container>
   );
 }
--- Gympoint/web/src/page/Students/styles.js
+++ Gympoint/web/src/page/Students/styles.js
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Container = styled.div`
-  border: 1px solid red;
   margin: 20px auto;
   width: 900px;
 `;
 
 export const ContentHead = styled.div`
-  border: 1px solid red;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -53,4 +51,56 @@ export const ContentHead = styled.div`
   }
 `;
 
-export const StudentsList = styled.main``;
+export const StudentsList = styled.main`
+  margin-top: 20px;
+  width: 100%;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  border-radius: 4px;
+  padding: 20px 10px;
+
+  table {
+    width: 100%;
+
+    thead th {
+      color: #444444;
+      text-align: left;
+      font-size: 16px;
+      font-weight: normal;
+    }
+
+    thead th:first-child {
+      padding-left: 10px;
+    }
+
+    td:last-child {
+      width: 20%;
+      text-align: right;
+      padding-right: 10px;
+    }
+
+    td:first-child {
+      padding-left: 10px;
+    }
+
+    tbody td {
+      padding: 12px 12px 12px 0;
+      border-bottom: 1px solid #eee;
+    }
+
+    button {
+      background: none;
+      border: none;
+      padding: 5px;
+    }
+  }
+`;
+
+export const BtnEdit = styled.button`
+  color: #4d85ee;
+`;
+export const BtnDelete = styled.button`
+  color: #de3b3b;
+`;
