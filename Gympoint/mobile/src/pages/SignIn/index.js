b7b917c306d2e2434e2acba4521bcf830878d450
Henrique <henrique.campaner@live.com>
2019-12-13 08:05:07 +0000

    ok

---
+++ Gympoint/mobile/src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Tex from '~/pages/HelpOrder'
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Tex />
+  );
+}
